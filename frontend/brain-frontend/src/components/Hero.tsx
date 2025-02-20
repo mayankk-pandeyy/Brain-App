b42c3e9ae93c481265f67010cc452c34be132598
Mayank Pandey <immayank7781@gmail.com>
2025-02-20 22:56:21 +0530

    Hero updated

--- frontend/brain-frontend/src/components/Hero.tsx
+++ frontend/brain-frontend/src/components/Hero.tsx
@@ -1,31 +1,36 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import { useContent } from "../hooks/useContent";
 import Card from "./Card"
+
+
 
 const Hero = () => {
 
-  useEffect(()=>{}, [])
+  const contents = useContent();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    if(contents.length > 0){
+      setLoading(false);
+    }
+  }, [contents])
+
 
   return (
     <div className="h-full flex items-center justify-center border border-[#555555]">
       <div className="h-[100%] w-[100%] overflow-y-scroll">
-        <div className="grid grid-cols-3 auto-rows-[400px] gap-4 py-5 px-2">
-          <div className="col-span-1 shadow-2xl bg-[#CAC6BC] hover:scale-95 cursor-pointer transition-all duration-100">
-            <Card title={"Project Ideas"} link={"https://www.youtube.com/embed/29yYlVm171c?si=smogJ5mWsDt0Bsoc"} type={"youtube"}/>
-          </div>
-          <div className="col-span-1 shadow-2xl hover:scale-95 cursor-pointer transition-all duration-100">
-            <Card title={"Hii"} link={"https://twitter.com/rishabhknows/status/1891714139849998621"} type={"twitter"}/>
-          </div>
-          <div className="col-span-1 shadow-2xl hover:scale-95 cursor-pointer transition-all duration-100">
-            <Card title={"Hii"} link={"https://twitter.com/mdevspy/status/1891606971779879408"} type={"twitter"}/>
-          </div>
-
-          <div className="col-span-1 shadow-2xl hover:scale-95 cursor-pointer transition-all duration-100">
-            <Card title={"Hii"} link={"https://twitter.com/mdevspy/status/1891606971779879408"} type={"twitter"}/>
-          </div>
-
-          <div className="col-span-1 shadow-2xl hover:scale-95 cursor-pointer transition-all duration-100">
-            <Card title={"Hii"} link={"https://twitter.com/mdevspy/status/1891606971779879408"} type={"twitter"}/>
-          </div>
+        <div className="py-5 px-2">
+          {
+            loading ? "Loading..." : (<div className="grid grid-cols-3 auto-rows-[400px] gap-4">
+              {
+                contents.map(({title})=>{
+                  return <div className="col-span-1 shadow-2xl bg-[#CAC6BC] hover:scale-95 cursor-pointer transition-all duration-100">
+                  <Card title={title} link={"https://www.youtube.com/embed/29yYlVm171c?si=smogJ5mWsDt0Bsoc"} type={"youtube"}/>
+                </div>
+                })
+              }
+            </div>)
+          }
         </div>
       </div>
     </div>
